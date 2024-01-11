69d3b356a87eb4a8db89d107ced04c9138381755
Helen Liu <mail2helenl@gmail.com>
2024-01-11 14:40:27 -0800

    modified zodiac file to not require initialization of year

--- zodiac.php
+++ zodiac.php
@@ -19,9 +19,8 @@
     ?>
 
     <?php
-        $remainder = 0;
-        $year = 0;
         extract($_POST);
+        echo "<p>You were born in the year of " . $year . ".</p>";
         $remainder = $year % 12;
         echo "<p>Your zodiac sign is " . $zodiac[$remainder] . ".</p>";
         echo "<img src='images/" . $zodiac[$remainder] . ".png' alt='" . $zodiac[$remainder] . "'>";
